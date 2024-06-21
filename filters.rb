0c5d030870c566981efc02d65fb397053a3ee376
Leilani <pquaby@gmail.com>
2024-06-20 20:42:16 -0400

    Add find by id method

--- filters.rb
+++ filters.rb
@@ -1,8 +1,11 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
-def find(id)
-    # Your code Here
+require './candidates'
+
+  def find(id)
+      # Returns the candidate with :id. If none, returns nil
+      result = @candidates.find {|candidate| candidate[:id] == id}
   end
   
   def experienced?(candidate)
@@ -14,3 +17,5 @@ def find(id)
   end
   
   # More methods will go below
+
+puts find(5)
